af05e785089c1ddddbf1367efb1ac5636579355c
Andre Matheus <am_arao@hotmail.com>
2022-04-12 18:19:41 -0300

    map (HastMap)

--- src/main/java/one/digitalinnovation/Main.java
+++ src/main/java/one/digitalinnovation/Main.java
@@ -29,5 +29,39 @@ public class Main {
         treeSetCarros.add(new Carro("Alfa Romeo"));
 
         System.out.println(treeSetCarros);
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+
+        Map<String, String> aluno = new HashMap<>();
+
+        aluno.put("Nome", "João");
+        aluno.put("Idade", "17");
+        aluno.put("Media", "8.5");
+        aluno.put("Turma", "3A");
+
+        System.out.println(aluno);
+
+        System.out.println(aluno.keySet());
+        System.out.println(aluno.values());
+
+        List<Map<String, String>>  listaAlunos = new ArrayList<>();
+
+        listaAlunos.add(aluno);
+
+        Map<String, String> aluno2 = new HashMap<>();
+
+        aluno.put("Nome", "Maria");
+        aluno.put("Idade", "18");
+        aluno.put("Media", "8.7");
+        aluno.put("Turma", "3B");
+
+        listaAlunos.add(aluno2);
+
+        System.out.println(listaAlunos);
+
+        System.out.println(aluno.containsKey("Nome"));
+
+
     }
 }
