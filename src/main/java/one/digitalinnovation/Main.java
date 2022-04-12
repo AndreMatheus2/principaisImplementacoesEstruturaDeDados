3c1a0ec15c37eacf8e85e5cf180ce6c34093a91e
Andre Matheus <am_arao@hotmail.com>
2022-04-12 17:35:56 -0300

    List (ArrayList)

--- src/main/java/one/digitalinnovation/Main.java
+++ src/main/java/one/digitalinnovation/Main.java
@@ -1,57 +1,23 @@
 package one.digitalinnovation;
 
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("");
-        System.out.println("");
-        System.out.println("      Stack a baixo      ");
-        System.out.println("");
-        System.out.println("");
+        List<Carro> listCarros = new ArrayList<>();
 
-        Stack<Carro> stackCarros = new Stack<>();
+        listCarros.add(new Carro("Ford"));
+        listCarros.add(new Carro("Chevrolet"));
+        listCarros.add(new Carro("Fiat"));
+        listCarros.add(new Carro("Peugeot"));
 
-        stackCarros.push(new Carro("Ford"));
-        stackCarros.push(new Carro("Chevrolet"));
-        stackCarros.push(new Carro("Fiat"));
+        System.out.println(listCarros.get(2));
+        System.out.println(listCarros.indexOf(new Carro("Fiat")));
+        System.out.println(listCarros.remove(2));
+        System.out.println(listCarros);
 
-        System.out.println(stackCarros);
-        System.out.println(stackCarros.pop());
-        System.out.println(stackCarros);
-        System.out.println(stackCarros.peek());
-        System.out.println(stackCarros);
-        System.out.println(stackCarros.empty());
-
-        System.out.println("");
-        System.out.println("");
-        System.out.println("      Queue a baixo      ");
-        System.out.println("");
-        System.out.println("");
-
-        Queue<Carro> queueCarros = new LinkedList<>();
-
-        queueCarros.add(new Carro("Ford"));
-        queueCarros.add(new Carro("Chevrolet"));
-        queueCarros.add(new Carro("Fiat"));
-
-        System.out.println(queueCarros.add(new Carro("Peugeot")));
-        System.out.println(queueCarros);
-
-        System.out.println(queueCarros.offer(new Carro("Renault")));
-        System.out.println(queueCarros);
-
-        System.out.println(queueCarros.poll());
-        System.out.println(queueCarros);
-
-
-        System.out.println(queueCarros.isEmpty());
-
-        System.out.println("O tamanho da fila é de= " + queueCarros.size());
     }
 }
