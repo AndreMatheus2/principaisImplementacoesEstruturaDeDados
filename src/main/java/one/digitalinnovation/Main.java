f84a99f1fcf8f744a2f1537b67cc952bc041caf4
Andre Matheus <am_arao@hotmail.com>
2022-04-12 17:24:39 -0300

    Queue

--- src/main/java/one/digitalinnovation/Main.java
+++ src/main/java/one/digitalinnovation/Main.java
@@ -1,11 +1,19 @@
 package one.digitalinnovation;
 
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        System.out.println("");
+        System.out.println("");
+        System.out.println("      Stack a baixo      ");
+        System.out.println("");
+        System.out.println("");
 
         Stack<Carro> stackCarros = new Stack<>();
 
@@ -20,5 +28,30 @@ public class Main {
         System.out.println(stackCarros);
         System.out.println(stackCarros.empty());
 
+        System.out.println("");
+        System.out.println("");
+        System.out.println("      Queue a baixo      ");
+        System.out.println("");
+        System.out.println("");
+
+        Queue<Carro> queueCarros = new LinkedList<>();
+
+        queueCarros.add(new Carro("Ford"));
+        queueCarros.add(new Carro("Chevrolet"));
+        queueCarros.add(new Carro("Fiat"));
+
+        System.out.println(queueCarros.add(new Carro("Peugeot")));
+        System.out.println(queueCarros);
+
+        System.out.println(queueCarros.offer(new Carro("Renault")));
+        System.out.println(queueCarros);
+
+        System.out.println(queueCarros.poll());
+        System.out.println(queueCarros);
+
+
+        System.out.println(queueCarros.isEmpty());
+
+        System.out.println("O tamanho da fila é de= " + queueCarros.size());
     }
 }
